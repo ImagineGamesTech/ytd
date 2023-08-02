2e278dd54636c3fc3c513e21e6a42edcc146b27a
Imagine Games <136746250+ImagineGamesTech@users.noreply.github.com>
2023-08-02 17:20:12 +0100

    Create ytdclient.py

---
+++ ytdclient.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+print("Loading YTD Client.")
+time.sleep(1)
+print("Loading YTD Client..")
+time.sleep(1)
+print("Loading YTD Client...")
+time.sleep(1)
+webbrowser.open('https://imaginegamestech.github.io/ytd')
